c19fedcbfe9cc81fb349a17717412b995c4a5b08
Antonio Nicoĺás <lan5432@hotmail.com>
2017-04-04 23:38:19 +0200

    Travis

--- src/test/java/steps/UpdateSteps.java
+++ src/test/java/steps/UpdateSteps.java
@@ -69,9 +69,10 @@ public class UpdateSteps {
     @And("^the interface has to be updated, including the list of most voted$")
     public void theInterfaceHasToBeUpdatedIncludingTheListOfMostVoted() throws Throwable {
         //Can't parse it another way
+        tester.sendTestProposal(prop);
         Thread.sleep(5000);
         result = mvc.perform(get("/")).andReturn();
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         assertTrue(result.getResponse().getContentAsString()
                 .contains("<h4>Proposals in the system: <span>1</span></h4>"));
     }
