e3384773344b09a7dfc174ab335551898a78516f
Antonio Nicoĺás <lan5432@hotmail.com>
2017-04-04 23:12:59 +0200

    Trying new fixes for Travis

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -3,6 +3,7 @@ import cucumber.api.junit.Cucumber;
 import dbmanagement.CommentsRepository;
 import dbmanagement.ParticipantsRepository;
 import dbmanagement.ProposalRepository;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 @CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest {
 
-    @Autowired
-    private ProposalRepository proposalRepo;
-    @Autowired
-    private ParticipantsRepository participantRepo;
-    @Autowired
-    private CommentsRepository commentRepo;
 
-    @Before
-    public void setUp() throws Exception {
-        proposalRepo.deleteAll();
-        participantRepo.deleteAll();
-        commentRepo.deleteAll();
-    }
 }
